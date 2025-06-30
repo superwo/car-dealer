e12c7ff1fe43de74439e9162391aa19ab8e82788
Radu <radu.avadanii@gmail.com>
2025-06-30 09:48:20 +0300

    initial files.'

---
+++ prisma/seed/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { seedImages } from "./images.seed";
+// import { seedClassifieds } from "./classifieds.seed";
+// import { seedTaxonomy } from "./taxonomy.seed";
+// import { seedAdmin } from "./admin.seed";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    // await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`;
+    // await prisma.$executeRaw`TRUNCATE TABLE "classifieds" RESTART IDENTITY CASCADE`;
+    // await seedTaxonomy(prisma);
+    // await seedClassifieds(prisma);
+    await seedImages(prisma);
+    // await seedAdmin(prisma);
+    // await seedCustomers(prisma);
+}
+
+main()
+    .catch((e) => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
